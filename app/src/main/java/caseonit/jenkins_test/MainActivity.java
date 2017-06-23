24d059030b733e40de11ba450c9a5232a83af252
Eduardo Giménez <eduardo@caseonit.com>
2017-06-23 21:14:56 +0200

    initial commit

---
+++ app/src/main/java/caseonit/jenkins_test/MainActivity.java
@@ -0,0 +1,23 @@
+package caseonit.jenkins_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Clase.
+ *
+ * @author egimenez eduardo@caseonit.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Método.
+     * @param savedInstanceState bundle
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/caseonit/jenkins_test/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * Contiene las clases relativas a la entrada a la aplicación. Como splash y
+ * pantalla inicial. En el futuro se incorporará una visualización en tiempo
+ * real de estado y funcionamiento de la app.
+ *
+ * @author egimenez
+ *
+ */
+package caseonit.jenkins_test;
